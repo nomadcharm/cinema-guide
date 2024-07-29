555f9440094ffd9b0cacc8a2b6c40e666c6e4eb4
Anna Yakubovich <yakubovichannie@gmail.com>
2024-07-29 03:34:43 +0300

    account page layout

--- src/pages/UserAccountPage/UserAccountPage.tsx
+++ src/pages/UserAccountPage/UserAccountPage.tsx
@@ -1,21 +1,28 @@
-import { useMutation } from '@tanstack/react-query';
-import { logoutUser } from '../../api/UserApi';
-import Layout from '../../components/Layout/Layout';
+import { useMutation } from "@tanstack/react-query";
+import { logoutUser } from "../../api/UserApi";
+import Layout from "../../components/Layout/Layout";
 import "./UserAccountPage.scss"
-import { queryClient } from '../../api/queryClient';
-import { useNavigate } from 'react-router';
-import { useContext } from 'react';
-import AuthContext from '../../context/AuthProvider';
+import { queryClient } from "../../api/queryClient";
+import { useNavigate } from "react-router";
+import { useContext, useEffect } from "react";
+import AuthContext from "../../context/AuthProvider";
+import { useSetPageTitle } from "../../hooks";
 
 const UserAccountPage = () => {
+  useSetPageTitle(`Мой аккаунт | Cinema Guide`)
+
   const navigate = useNavigate();
 
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser, getCurrentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
 
   const logoutMutation = useMutation({
     mutationFn: () => logoutUser(),
     onSuccess() {
-      queryClient.invalidateQueries({ queryKey: ["users", "current"], })
+      // queryClient.invalidateQueries({ queryKey: ["users", "current"], })
       navigate("/")
     }
   }, queryClient)
@@ -26,16 +33,46 @@ const UserAccountPage = () => {
 
   return (
     <Layout>
-      <section className="account">
-        <div className="container">
+      <section className="user-account">
+        <div className="container user-account__container">
           {
-            currentUser && <p>User: <span>{currentUser.name}</span></p>
-          }
+            currentUser ?
+              <>
+                <h2 className="page-title user-account__title">Мой аккаунт</h2>
 
-          <button
-            className="button button-primary"
-            onClick={() => handleLogout()}
-          >Выйти</button>
+                <ul className="user-account__nav">
+                  <li className="user-account__nav-item">
+                    <h3 className="user-account__tab user-account__tab--favourites">Избранные фильмы</h3>
+                  </li>
+                  <li className="user-account__nav-item">
+                    <h3 className="user-account__tab active user-account__tab--settings">Настройка аккаунта</h3>
+                  </li>
+                </ul>
+
+                <div className="user-account__content">
+                  <div className="user-account__info">
+                    <p className="user-account__initials">
+                      {currentUser.name.substring(0, 1)}{currentUser.surname.substring(0, 1)}
+                    </p>
+                    <div>
+                      <p className="user-account__category">Имя Фамилия</p>
+                      <p className="user-account__details">{currentUser.name} {currentUser.surname}</p>
+                    </div>
+                  </div>
+                  <div className="user-account__info user-account__info--email">
+                    <div>
+                      <p className="user-account__category">Электронная почта</p>
+                      <p className="user-account__details">{currentUser.email}</p>
+                    </div>
+                  </div>
+                </div>
+                <button
+                  className="button button-primary"
+                  onClick={() => handleLogout()}
+                >Выйти из аккаунта</button>
+              </> :
+              <img src="https://media.tenor.com/_BiwWBWhYucAAAAe/what-huh.png" alt="" />
+          }
         </div>
       </section>
     </Layout>
