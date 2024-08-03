6670355a8981bcbc81350e7954ae07eba281ab5e
Anna Yakubovich <yakubovichannie@gmail.com>
2024-08-03 14:04:35 +0300

    Updates

--- assets/MainLoader-CmOt9_07.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-Cmeav6R9.js";const r=()=>a.jsx("div",{className:"loader",children:a.jsx("div",{className:"loader__inner"})});export{r as default};
---
+++ assets/MainLoader-Dth-G7fL.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-BL0Y5HU2.js";const r=()=>a.jsx("div",{className:"loader",children:a.jsx("div",{className:"loader__inner"})});export{r as default};
