ead226b82c07281a2fe256f77c13d910ddd74477
Anna Yakubovich <yakubovichannie@gmail.com>
2024-08-09 18:06:57 +0300

    auth modal optimization

--- src/components/AuthModal/AuthModal.tsx
+++ src/components/AuthModal/AuthModal.tsx
@@ -1,14 +1,17 @@
-import { FC, useContext } from "react";
+import { FC, lazy, Suspense, useContext } from "react";
 import { ReactSVG } from "react-svg";
-import RegistrationForm from "../RegistrationForm/RegistrationForm";
+// import RegistrationForm from "../RegistrationForm/RegistrationForm";
 import LoginForm from "../LoginForm/LoginForm";
 import { close, logo } from "../../assets/assets";
 import AuthContext from "../../context/AuthProvider";
 import "./AuthModal.scss";
+import MainLoader from "../Loaders/MainLoader/MainLoader";
 
 interface AuthModalProps {
   isOpen: boolean;
 }
+
+const RegistrationForm = lazy(() => import("../RegistrationForm/RegistrationForm"));
 
 const AuthModal: FC<AuthModalProps> = ({ isOpen }) => {
   const { authMode, handleAuthModalCall, handleClick } = useContext(AuthContext);
@@ -29,7 +32,14 @@ const AuthModal: FC<AuthModalProps> = ({ isOpen }) => {
           <ReactSVG src={close} />
         </button>
         <img className="auth-modal__logo" src={logo} alt="Cinema Guide" width={180} />
-        {authMode === "register" ? <RegistrationForm /> : <LoginForm />}
+        {
+          authMode === "register"
+            ? <Suspense fallback={<MainLoader />}>
+              <RegistrationForm />
+            </Suspense>
+            : <LoginForm
+            />
+        }
         <button className="auth-modal__button" onClick={() => handleClick()}>
           {authMode === "register" ? "У меня есть пароль" : "Регистрация"}
         </button>
