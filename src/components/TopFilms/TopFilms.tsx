c7d1edcef54e77d809a24a2d5332539aa4d74109
Anna Yakubovich <yakubovichannie@gmail.com>
2024-08-12 02:48:24 +0300

    media queries changes

--- src/components/TopFilms/TopFilms.tsx
+++ src/components/TopFilms/TopFilms.tsx
@@ -12,16 +12,18 @@ const TopFilms: FC = (): ReactElement => {
         <h2 className="top-films__title section-title">Топ 10 фильмов</h2>
         {
           topFilms ? (
-            <ul className="top-films__list">
-              {
-                topFilms.map((film, index) => {
-                  return <li className="top-films__item" key={film.id}>
-                    <p className="top-films__number">{index + 1}</p>
-                    <FilmPreviewCard id={film.id} title={film.title} posterUrl={film.posterUrl} />
-                  </li>
-                })
-              }
-            </ul>
+            <div className="list-wrapper">
+              <ul className="top-films__list">
+                {
+                  topFilms.map((film, index) => {
+                    return <li className="top-films__item" key={film.id}>
+                      <p className="top-films__number">{index + 1}</p>
+                      <FilmPreviewCard id={film.id} title={film.title} posterUrl={film.posterUrl} />
+                    </li>
+                  })
+                }
+              </ul>
+            </div>
           ) : null
         }
       </div>
