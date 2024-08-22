8829dc3188a3a5a514ce1cb9ec6d82224d6cd4e8
Anna Yakubovich <yakubovichannie@gmail.com>
2024-08-22 16:58:21 +0300

    film preview card title addition

--- src/components/FilmPreviewCard/FilmPreviewCard.tsx
+++ src/components/FilmPreviewCard/FilmPreviewCard.tsx
@@ -1,15 +1,18 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { FilmPreview } from "../../models/FilmSchemas";
 import { cardStub } from "../../assets/assets";
 import "./FilmPreviewCard.scss";
 
-const FilmPreviewCard: FC<FilmPreview> = ({ id, title, posterUrl }): ReactElement => {
-
+const FilmPreviewCard: FC<FilmPreview> = ({ id, title, posterUrl }): ReactNode => {
+  
   return (
     <article className="film-card">
       <Link className="film-card__link" to={`/movie/${id}`} >
-        <img className="film-card__img" src={posterUrl ? posterUrl: cardStub} alt={title} loading="lazy" width={224} height={336} />
+        <img className="film-card__img" src={posterUrl ? posterUrl: cardStub} alt={title} width={224} height={336} />
+        {
+          !posterUrl && <p className="film-card__title">{title}</p>
+        }
       </Link>
     </article>
   );
