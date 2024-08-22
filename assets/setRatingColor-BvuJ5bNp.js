804f0b9d6f998423b18ce4d66219e046d5cc8593
Anna Yakubovich <yakubovichannie@gmail.com>
2024-08-23 02:06:52 +0300

    Updates

---
+++ assets/setRatingColor-BvuJ5bNp.js
@@ -0,0 +1,1 @@
+const o=s=>{const e=Math.trunc(s/60),t=s%60;return`${e} ч ${t} мин`},l=s=>{let e="rating";return s>=8.5?e+=" excellent":s>=7?e+=" high":s>=6?e+=" medium":e+=" low",e};export{o as f,l as s};
