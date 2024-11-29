85400ba39d2234ae8cd5554e872470452042c3c8
Anna Yakubovich <yakubovichannie@gmail.com>
2024-11-29 19:39:40 +0300

    Updates

---
+++ assets/Button-BfdetmQg.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C83grKiv.js";const n=()=>t.jsx("div",{className:"loader-btn"}),a=({isLoading:r,children:s,...o})=>t.jsx("button",{...o,children:r?t.jsx(n,{}):s});export{a as B};
